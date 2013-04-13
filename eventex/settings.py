4ba76d4fe7ee5f327253c9675bf8014ed74d8cd7
Henrique Bastos <henrique@bastos.net>
2013-04-13 10:08:00 -0300

    Configure database for syncdb

--- eventex/settings.py
+++ eventex/settings.py
@@ -1,4 +1,6 @@
 # Django settings for eventex project.
+from unipath import Path
+PROJECT_DIR = Path(__file__).parent
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -11,8 +13,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': PROJECT_DIR.child('database.db'),
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
