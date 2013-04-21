3c97018ac2bf3c2d181e9e0019e448a53f904507
Henrique Bastos <henrique@bastos.net>
2013-04-20 22:18:01 -0300

    Add route to global urlconf

--- eventex/urls.py
+++ eventex/urls.py
@@ -4,8 +4,9 @@ from django.conf.urls import patterns, include, url
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('eventex.core.views',
-    url(r'^$', 'homepage', name='homepage'),
+urlpatterns = patterns('',
+    url(r'^$', 'eventex.core.views.homepage', name='homepage'),
+    url(r'^inscricao/$', 'eventex.subscriptions.views.subscribe', name='subscribe'),
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
