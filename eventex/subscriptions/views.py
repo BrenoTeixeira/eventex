36958ad71435e7722c80a7d37be30b12c12909d8
Henrique Bastos <henrique@bastos.net>
2013-04-21 08:21:24 -0300

    Add get_object_or_404 to detail

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from eventex.subscriptions.forms import SubscriptionForm
 from eventex.subscriptions.models import Subscription
 
@@ -26,5 +26,5 @@ def create(request):
 
 
 def detail(request, pk):
-    subscription = Subscription.objects.get(pk=pk)
+    subscription = get_object_or_404(Subscription, pk=pk)
     return render(request, 'subscriptions/subscription_detail.html', {'subscription': subscription})
